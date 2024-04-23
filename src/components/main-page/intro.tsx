b4a4aa425a131a9467516fc8d9ccdc7948040b41
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2024-04-23 12:37:02 +0200

    check overflow

--- src/components/main-page/intro.tsx
+++ src/components/main-page/intro.tsx
@@ -3,17 +3,46 @@ import React from 'react';
 
 const Intro = () => {
 	return (
-		<section className='relative flex justify-center items-center mb-[5rem] shadow-xl shadow-black'>
-			<Image
-				src='/intro.jpg'
-				alt='logo'
-				width={1980}
-				height={808}
-				priority
-				className='w-[100vw] '
-			/>
-			<div className='bg-white bg-opacity-85 absolute flex justify-center flex-col items-center w-[40%] p-8 shadow-xl shadow-black rounded-xl'>
-				<p className='text-2xl font-semibold leading-10'>
+		<section className='relative flex justify-center items-center mb-[5rem] min-h-[85vh]'>
+			<div className='h-[85vh]'>
+				<Image
+					src='/intro.jpg'
+					alt='logo'
+					width={1980}
+					height={808}
+					priority
+					className='hidden sm:inline object-cover shadow-xl shadow-black'
+				/>
+				<Image
+					src='/intro.jpg'
+					alt='logo'
+					sizes='85vh'
+					fill
+					priority
+					className='object-cover sm:hidden shadow-xl shadow-black'
+				/>
+			</div>
+			{/* <div className='max-h-[100vh]'>
+				<Image
+					src='/background.jpg'
+					alt='backgorund'
+					sizes='100vh'
+					quality={80}
+					fill
+					priority
+					className='hidden sm:inline object-cover z-[-3] '
+				/>
+				<Image
+					src='/background-mobile.jpg'
+					alt='backgorund-mobile'
+					fill
+					quality={80}
+					priority
+					className='sm:hidden z-[-3] object-cover'
+				/>
+			</div> */}
+			{/* <div className='bg-white bg-opacity-85 absolute flex justify-center flex-col items-center w-[40%] p-8 shadow-xl shadow-black rounded-xl'>
+				<p className='sm:text-2xl font-semibold leading-10'>
 					Witaj w{' '}
 					<span className='text-main-color'>SmartEat Advisor</span> -
 					Twoim wszechstronnym przewodniku po zdrowym odżywianiu!{' '}
@@ -25,14 +54,14 @@ const Intro = () => {
 					aplikacja pozwalająca kontrolować prawidłowe poziomy kalori
 					oraz wszystkich mikro i makroskładników.
 				</p>
-			</div>
+			</div> */}
 			<Image
 				src='/pomidor.png'
 				alt=''
 				width={300}
 				height={295}
 				priority
-				className='w-[24rem] absolute bottom-[-10rem] left-[-5rem] z-10'
+				className='w-[8rem] sm:w-[24rem] absolute bottom-[-10rem] left-[-5rem] z-10'
 			/>
 			<Image
 				src='/zioła.png'
@@ -40,7 +69,7 @@ const Intro = () => {
 				width={600}
 				height={600}
 				priority
-				className='w-[42rem] absolute bottom-[-32rem] right-[-16rem] rotate-[-30deg] z-10'
+				className='w-[14rem] sm:w-[42rem] bottom-[-8rem] absolute sm:bottom-[-32rem] right-[-5rem] sm:right-[-16rem] rotate-[-30deg] z-10'
 			/>
 		</section>
 	);
